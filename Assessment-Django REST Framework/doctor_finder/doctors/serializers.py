ba8e20cc2fa769e08fbfb8460bc6ca2bf79c2431
Khursheed-Khan86 <misskhursheedkhan@gmail.com>
2026-05-14 20:38:31 +0530

    Add files via upload
    Done

---
+++ Assessment-Django REST Framework/doctor_finder/doctors/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Doctor
+
+class DoctorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+
+
+    # Validation
+    def validate_experience(self, value):
+
+        if value < 0:
+            raise serializers.ValidationError(
+                "Experience cannot be negative"
+            )
+
+        return value
---
+++ Assessment-Django REST Framework/doctor_finder/doctors/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets
+from django.db import transaction
+
+from .models import Doctor
+from .serializers import DoctorSerializer
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+
+    queryset = Doctor.objects.all()
+
+    serializer_class = DoctorSerializer
+
+    ordering_fields = ['name', 'specialization']
+
+
+    # Atomic Transaction
+    def perform_create(self, serializer):
+
+        with transaction.atomic():
+
+            serializer.save()
+
+
+    def perform_update(self, serializer):
+
+        with transaction.atomic():
+
+            serializer.save()
+
+# Create your views here.
